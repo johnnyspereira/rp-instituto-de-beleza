3b47d68ecb58745086ccf6cc5cbab7e143c6f9da
johnnyspereira <johnny.stephane@gmail.com>
2026-09-18 16:11:50 +0100

    Remove stale WhatsApp credential seed

--- mysql/migrations/026_whatsapp_worker_credentials.sql
+++ mysql/migrations/026_whatsapp_worker_credentials.sql
@@ -8,10 +8,3 @@ CREATE TABLE IF NOT EXISTS whatsapp_worker_credentials (
   CONSTRAINT whatsapp_worker_credentials_account_fk
     FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO whatsapp_worker_credentials(account_id, secret_hash)
-VALUES(
-  '999933bb-5873-4612-abaf-40db59ca6ffc',
-  'fe1849aa9e65f67ed7f0379a655c539e47a577542ba3c4505140b0ed2c10ecdb'
-)
-ON DUPLICATE KEY UPDATE secret_hash = VALUES(secret_hash);
